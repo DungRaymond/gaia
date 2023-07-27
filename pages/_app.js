7fc80c45762022b4675b48939f28e992dda45f22
yvesNg <45696769+DungRaymond@users.noreply.github.com>
2023-07-27 14:51:51 +0700

    fast commit

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ import '../styles/globals.css';
 import Link from 'next/link';
 import { createTheme } from '@mui/material/styles';
 import DetectScreenWidth from '@/utility/detectScreenWidth';
-
+import Head from 'next/head';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -20,85 +20,91 @@ function App(props) {
 
 
   return (
-  <CacheProvider value={emotionCache}>
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <header>
-        <Grid container spacing={2} alignItems="center" justifyContent="center" border={'1px solid red'}>
+    <>
+      <Head>
+        <link rel="icon" href='/public/gaia-website-favicon-color.png' type='image' />
+      </Head>
+    
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <header>
+            <Grid container spacing={2} alignItems="center" justifyContent="center" border={'1px solid red'}>
 
-          <Grid item container spacing={2} xs={6} sm={6} md={4} lg={3} alignItems="center" justifyContent="flex-start" border={'1px solid green'}>
-            <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
-              <Link href='/'>
-                <p id='headBar_phone' className='headBar_text'>
-                  <PhoneIcon className='headBar_text_icon' fontSize='0.9em'></PhoneIcon>
-                  <span>
-                    0334466664
-                  </span>
-                </p>
-              </Link>
-            </Grid>
-            <Grid item xs={5} sm={7} md={7} lg={7}>
-                <Link href='/'>
-                  <p id='headBar_mail' className='headBar_text'>
-                    <MailIcon className='headBar_text_icon' fontSize='1.4em'></MailIcon>
-                    <span>
-                      info@gaiacosmetics.vn
-                    </span>
-                  </p>
-                </Link>
-            </Grid>
-          </Grid>
+              <Grid item container spacing={2} xs={6} sm={6} md={4} lg={3} alignItems="center" justifyContent="flex-start" border={'1px solid green'}>
+                <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
+                  <Link href='/'>
+                    <p id='headBar_phone' className='headBar_text'>
+                      <PhoneIcon className='headBar_text_icon' fontSize='0.9em'></PhoneIcon>
+                      <span>
+                        0334466664
+                      </span>
+                    </p>
+                  </Link>
+                </Grid>
+                <Grid item xs={5} sm={7} md={7} lg={7}>
+                    <Link href='/'>
+                      <p id='headBar_mail' className='headBar_text'>
+                        <MailIcon className='headBar_text_icon' fontSize='1.4em'></MailIcon>
+                        <span>
+                          info@gaiacosmetics.vn
+                        </span>
+                      </p>
+                    </Link>
+                </Grid>
+              </Grid>
 
-          <Grid container item spacing={2} xs={6} sm={4} md={4} lg={4} alignItems="center" justifyContent="flex-end" border={'1px solid yellow'}>
-            <Grid item sm={3}>
-              <Link href='/'>
-                <p className='headBar_text'>
-                  Hệ thống cửa hàng
-                </p>
-              </Link>
+              <Grid container item spacing={2} xs={6} sm={4} md={4} lg={4} alignItems="center" justifyContent="flex-end" border={'1px solid yellow'}>
+                <Grid item sm={3}>
+                  <Link href='/'>
+                    <p className='headBar_text'>
+                      Hệ thống cửa hàng
+                    </p>
+                  </Link>
+                </Grid>
+                <Grid item sm={3}>
+                  <Link href='/'>
+                    <p className='headBar_text'>
+                      Theo dõi đơn hàng
+                    </p>
+                  </Link>
+                </Grid>
+                <Grid item sm={2}>
+                  <Link href='/'>
+                    <p className='headBar_text'>
+                      Tài khoản
+                    </p>
+                  </Link>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item sm={3}>
-              <Link href='/'>
-                <p className='headBar_text'>
-                  Theo dõi đơn hàng
-                </p>
-              </Link>
-            </Grid>
-            <Grid item sm={2}>
-              <Link href='/'>
-                <p className='headBar_text'>
-                  Tài khoản
-                </p>
-              </Link>
-            </Grid>
-          </Grid>
-        </Grid>
 
-        <style jsx>
-          {`
-            .headBar_text {
-              font-size: 1em;
-              display: flex;
-              align-items: center;
-              padding: 6px 2px;
-            }
+            <style jsx>
+              {`
+                .headBar_text {
+                  font-size: 1em;
+                  display: flex;
+                  align-items: center;
+                  padding: 6px 2px;
+                }
 
-            .headBar_text_icon {
-              font-size: 
-            }
+                .headBar_text_icon {
+                  font-size: 
+                }
 
-            .headBar_text > span {
-              
-            }
-          `}
-        </style>
-      </header>
-      <Component {...pageProps} />
-      <footer>
-        <h1>this is footer</h1>
-      </footer>
-    </ThemeProvider>
-  </CacheProvider>
+                .headBar_text > span {
+                  
+                }
+              `}
+            </style>
+          </header>
+          <Component {...pageProps} />
+          <footer>
+            <h1>this is footer</h1>
+          </footer>
+        </ThemeProvider>
+      </CacheProvider>
+    </>
   )
 };
 
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <title>Create Next App</title>
         <meta name="description" content="Gaia Cosmeceuticals" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/public/png/logo-no-background.png" />
+        <link rel="icon" href="/gaia-website-favicon-color.ico" type="image/gif" sizes='16x16' />
       </Head>
       <main className={styles.main}>
         <h1> build </h1>
